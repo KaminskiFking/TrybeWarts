1cf8091e1341196cdf9b145183a11d66bab48dcf
KaminskiFking <vikaminski@hotmail.com>
2022-06-07 18:59:38 -0300

    Requisitos Obrigatorios Completos

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const emailCheck = document.getElementById('email');
 const loginCheck = document.getElementById('senha');
 const buttonCheck = document.getElementById('buttonLogin');
+const checkBoxCheck = document.getElementById('agreement');
+const buttonStart = document.querySelector('#submit-btn');
 
 function clickLogin() {
   if (emailCheck.value === 'tryber@teste.com' && loginCheck.value === '123456') {
@@ -11,3 +13,10 @@ function clickLogin() {
 }
 
 buttonCheck.addEventListener('click', clickLogin);
+
+function checkBoxSelected() {
+  if (checkBoxCheck.checked) {
+    buttonStart.removeAttribute('disabled');
+  }
+}
+checkBoxCheck.addEventListener('click', checkBoxSelected);
